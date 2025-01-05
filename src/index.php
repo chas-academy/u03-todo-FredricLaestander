1fb57e442f45e9d3d3a4521b65bc7f489ce4db2b
Fredric Laestander <fredriclaestander@gmail.com>
2025-01-05 17:57:17 +0100

    Add SASS styling and search function

--- src/index.php
+++ src/index.php
@@ -1,66 +1,70 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
 
-    <body>
-        <aside>
-            <nav>
-                <!-- sökfält för att söka efter task rubriker -->
-                <a href="">
-                    <img src="assets/icons/sun.svg" alt="sun">My day
-                </a>
-                <a href=""><img src="assets/icons/calendar.svg" alt="calendar">Scheduled plans
-                </a>
-                <a href="">
-                    <img src="assets/icons/pin.svg" alt="pin">Important
-                </a>
-                <a href="">
-                    <img src="assets/icons/folder.svg" alt="folder">All assignements
-                </a>
-            </nav>
-        </aside>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles/styles.css">
+  <title>Document</title>
+</head>
 
-        <main>
-            <section>
-                <H1>Headline here</H1>
-                <!-- Hämta ut från databas -->
-                <!-- 9th of december, monday -->
-                <p>day of month, weekday</p>
-            </section>
+<body>
+  <aside>
+    <nav>
+      <!-- sökfält för att söka efter task rubriker -->
+      <a href="">
+        <img src="assets/icons/sun.svg" alt="sun">My day
+      </a>
+      <a href=""><img src="assets/icons/calendar.svg" alt="calendar">Scheduled plans
+      </a>
+      <a href="">
+        <img src="assets/icons/pin/pin-blue.svg" alt="pin">Important
+      </a>
+      <a href="">
+        <img src="assets/icons/folder.svg" alt="folder">All assignements
+      </a>
+    </nav>
+  </aside>
 
-            <div class="tasks">
-                <div class="task">
-                    <div class="content">
-                        <img src="assets/icons/checked/checked_false.svg" alt="not checked icon">
-                        <div class="taskhead">
-                            <h2>Title</h2>
-                            <div class="datetime">
-                                <p>location:</p>
-                                <p>hour:minute</p>
-                            </div>
-                        </div>
-                    </div>
+  <main>
+    <header>
+      <img src="assets/icons/menu.svg" alt="menu">
 
-                    <!-- task description -->
-                    <p>Text</p>
+      <div class="text">
+        <h1>Headline here</h1>
+        <p><?php echo date("F j, l") ?></p>
+      </div>
+    </header>
 
-                    <div class="tools">
-                        <img src="assets/icons/pen.svg" alt="edit not checked">
-                        <img src="assets/icons/pin.svg" alt="important not checked">
-                    </div>
-                </div>
+    <div class="tasks">
+      <details class="task">
+        <summary>
+          <div class="content">
+            <div class="checked"></div>
+            <div class="taskhead">
+              <h2>Title</h2>
+              <div class="datetime">
+                <p>location:</p>
+                <p>hour:minute</p>
+              </div>
             </div>
+          </div>
 
-            <div class="createtask">
-                <img src="assets/icons/plus.svg" alt="add task not checked">
-                <img src="assets/icons/ellipse.svg" alt="add task checked">
-                <p>Add assignements...</p>
-            </div>
-        </main>
-    </body>
+          <div class="tools">
+            <div class="pen"></div>
+            <div class="pin"></div>
+          </div>
+        </summary>
+
+        <p class="description">Text</p>
+      </details>
+    </div>
+
+    <div class="createtask">
+      <img src="assets/icons/plus.svg" alt="add task not checked">
+      <input type="text" placeholder="Add an assignment..." name="task" />
+    </div>
+  </main>
+</body>
+
 </html>
-    
